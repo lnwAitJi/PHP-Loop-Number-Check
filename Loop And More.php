c00a7f43e4efb56d3d79926682213ec7fa57c9a9
Inw AitJi <100911929+lnwAitJi@users.noreply.github.com>
2022-06-11 17:58:27 +0700

    Update.png
    Thank pooooooon(kanison konkla)

--- Loop And More.php
+++ Loop And More.php
@@ -1,35 +1,34 @@
 <!-- Remember To Stop Press "Ctrl + C" In Terminal -->
-
 <?php
 /**  This Is Just Readline */
 function readline($prompt = ""):string{if($prompt){if(is_array($prompt)){var_dump($prompt);}else {echo $prompt."";}}$fp = fopen("php://stdin","r");$line = rtrim(fgets($fp, 1024));if(empty($line)){return '';}return $line;}
 # --- Input!! --- #
 Warp:
-$Input = readline("Loop Time? > ");                # Ask Loop Time #
-if ( is_numeric($Input) ) {                        # Is number #
+$Input = (int)readline("Loop Time? > ");           # Ask Loop Time #
+$Input2 = null;
+if (is_numeric($Input) ) {                         # Is number #
     $Input2 = readline("Start At? > ");            # Ask Start At #
-    if ( is_numeric($Input2) ) {                   # Is Number #
+    if (is_numeric($Input2) ) {                    # Is Number #
         $start = $Input2;                          # Start = Input2 #
         # It okay Next #
     } else {                                       # Else #
         echo("'".$Input2."' Is not number!!\n");   # Say Not Number #
+        $start = $Input2 = null;
     }
 } else {                                           # Else #
     echo("'".$Input."' Is not number!!\n");        # Say Not Number #
+    unset($Input, $Input2);
     goto Warp;                                     # Go to Warp #
 }
 if ($start <= 0){
-    $start = 0;
     echo("Bruh it less 0\n");
     echo("Set $start to 0 . .\n");
+    $start = 0;
     sleep(2);
 } else {
-    $start = $Input2;
+    $start = $Input2 ?? 0;
 }
-
-for ($loop = $start; $loop <= $Input; $loop+=1) {
-    echo "Number > $loop \n";
-    }
-
-?>
-<!-- -- Thank For Vist My Code :) -- -->
+for ($loop = 0; $start !== $Input; $loop++) {
+    echo("Number > $loop \n");
+}
+##!-- -- Thank For Vist My Code :) -- --
